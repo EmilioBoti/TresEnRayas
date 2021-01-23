ce54f98ad5c603728079a13700ce71679ff65502
unknown <emiliobotier@gmail.com>
2021-01-23 14:19:45 +0100

    optimized

--- js/checkWinner.js
+++ js/checkWinner.js
@@ -36,87 +36,45 @@ function theWinner(){
   let boardPlayer1 = document.getElementById("player1")
   let boardPlayer2 = document.getElementById("player2")
   let sayWinner = ""
-
-  //line 1
-  
-  if((matrixPlay[0][0] === matrixPlay[0][1]) && (matrixPlay[0][0] === matrixPlay[0][2])){
-    win = true
-    if(matrixPlay[0][0] === "X"){
-     player1++
-     sayWinner = matrixPlay[0][0]
-     boardPlayer1.innerHTML = player1
-    }else{
-     player2++
-     sayWinner = matrixPlay[0][0]
-     boardPlayer2.innerHTML = player2
+  const size = matrixPlay.length
+  console.log(size)
+  for(let f = 0; f < size; f++){
+    if((matrixPlay[f][0] === matrixPlay[f][1]) && (matrixPlay[f][1] === matrixPlay[f][2])){
+      win = true
+      if(matrixPlay[f][0] === "X"){
+          console.log(matrixPlay)
+          player1++
+          sayWinner = matrixPlay[f][0]
+          boardPlayer1.innerHTML = player1
+      }else{
+        if(matrixPlay[f][0] === "O"){
+          console.log(matrixPlay)
+          player2++
+          sayWinner = matrixPlay[f][0]
+          boardPlayer2.innerHTML = player2
+        }
+      }
     }
-  }
- //line middle
-  if( (matrixPlay[1][0] === matrixPlay[1][1]) && (matrixPlay[1][1] === matrixPlay[1][2])){
-    win = true
-    if(matrixPlay[1][0] === "X"){
-     player1++
-     sayWinner = matrixPlay[1][0]
-     boardPlayer1.innerHTML = player1
-    }else{
-     player2++
-     sayWinner = matrixPlay[1][0]
-     boardPlayer2.innerHTML = player2
-    }
-  }
- // line 3
-  if( (matrixPlay[2][0] === matrixPlay[2][1]) && (matrixPlay[2][1] === matrixPlay[2][2])){
-    win = true
-    if(matrixPlay[2][0] === "X"){
-     player1++
-     sayWinner = matrixPlay[2][0]
-     boardPlayer1.innerHTML = player1
-    }else{
-     player2++
-     sayWinner = matrixPlay[2][0]
-     boardPlayer2.innerHTML = player2
-    }
-  }
- //column 1
-  if((matrixPlay[0][0] === matrixPlay[1][0]) && (matrixPlay[0][0] === matrixPlay[2][0])){
-    win = true
-    if(matrixPlay[0][0] === "X"){
-     player1++
-     sayWinner = matrixPlay[0][0]
-     boardPlayer1.innerHTML = player1
-    }else{
-     player2++
-     sayWinner = matrixPlay[0][0]
-     boardPlayer2.innerHTML = player2
-    }
-  }
-//Column middle
-  if( (matrixPlay[0][1] === matrixPlay[1][1]) && (matrixPlay[0][1] === matrixPlay[2][1])){
-    win = true
-    if(matrixPlay[0][1] === "X"){
-    player1++
-    sayWinner = matrixPlay[0][1]
-    boardPlayer1.innerHTML = player1
-    }else{
-    player2++
-    sayWinner = matrixPlay[0][1]
-    boardPlayer2.innerHTML = player2
-    }
-  }
-   //Column 3
-  if( (matrixPlay[0][2] === matrixPlay[1][2]) && (matrixPlay[0][2] === matrixPlay[2][2])){
-    win = true
-    if(matrixPlay[0][2] === "X"){
-     player1++
-     sayWinner = matrixPlay[0][2]
-     boardPlayer1.innerHTML = player1
-    }else{
-     player2++
-     sayWinner = matrixPlay[0][2]
-     boardPlayer2.innerHTML = player2
-     }
   }
 
+  for(let f = 0; f < size; f++){
+    if((matrixPlay[0][f] === matrixPlay[1][f]) && (matrixPlay[0][f] === matrixPlay[2][f])){
+      win = true
+      if(matrixPlay[0][f] === "X"){
+          console.log(matrixPlay)
+          player1++
+          sayWinner = matrixPlay[0][f]
+          boardPlayer1.innerHTML = player1
+      }else{
+        if(matrixPlay[0][f] === "O"){
+          console.log(matrixPlay)
+          player2++
+          sayWinner = matrixPlay[0][f]
+          boardPlayer2.innerHTML = player2
+        }
+      }
+    }
+  }
 //diagonal \
   if( (matrixPlay[0][0] === matrixPlay[1][1]) && (matrixPlay[0][0] === matrixPlay[2][2])){
     win = true
